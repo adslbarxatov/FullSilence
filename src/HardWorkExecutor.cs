349922154f5e4654d9136f3e765bbf0579a2850b
Nicolay B. aka RD_AAOW <adslbarxatov@mail.ru>
2019-08-30 22:35:24 +1100

    DP assembly v 7.4.217.0

--- src/HardWorkExecutor.cs
+++ src/HardWorkExecutor.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace ESHQSetupStub
@@ -12,6 +13,10 @@ namespace ESHQSetupStub
 #if !SIMPLE_HWE
 		// Переменные
 		private bool allowClose = false;
+		private Bitmap progress, frameGreenGrey, frameBack;
+		private int currentPercentage = 0;
+		private Graphics g, gp;
+		private int currentOffset = 0;
 #endif
 
 		/// <summary>
@@ -58,8 +63,8 @@ namespace ESHQSetupStub
 			argument.Add (Uninstall.ToString ());
 
 			// Настройка контролов
-			this.BackColor = MainProgress.BackColor = ProgramDescription.MasterBackColor;
-			StateLabel.ForeColor = AbortButton.ForeColor = MainProgress.ForeColor = ProgramDescription.MasterTextColor;
+			this.BackColor = ProgramDescription.MasterBackColor;
+			StateLabel.ForeColor = AbortButton.ForeColor = ProgramDescription.MasterTextColor;
 			AbortButton.BackColor = ProgramDescription.MasterButtonColor;
 
 			// Настройка BackgroundWorker
@@ -70,7 +75,62 @@ namespace ESHQSetupStub
 			bw.ProgressChanged += ProgressChanged;
 			bw.RunWorkerCompleted += RunWorkerCompleted;
 
-			// Запуск
+			// Инициализация ProgressBar
+			progress = new Bitmap (this.Width - 20, 30);
+			g = Graphics.FromHwnd (this.Handle);
+			gp = Graphics.FromImage (progress);
+
+			// Формирование стрелок
+			Point[] frame = new Point[] {
+					new Point (0, 0),
+					new Point (this.Width / 4, 0),
+					new Point (this.Width / 4 + progress .Height / 2, progress .Height / 2),
+					new Point (this.Width / 4, progress .Height),
+					new Point (0, progress .Height),
+					new Point (progress .Height / 2, progress .Height / 2)
+					};
+
+			// Подготовка дескрипторов
+			SolidBrush green = new SolidBrush (Color.FromArgb (0, 160, 80)),
+				grey = new SolidBrush (Color.FromArgb (160, 160, 160)),
+				back = new SolidBrush (this.BackColor);
+
+			frameGreenGrey = new Bitmap (10 * this.Width / 4, progress.Height);
+			frameBack = new Bitmap (10 * this.Width / 4, progress.Height);
+			Graphics g1 = Graphics.FromImage (frameGreenGrey),
+				g2 = Graphics.FromImage (frameBack);
+
+			// Сборка
+			for (int i = 0; i < 8; i++)
+				{
+				for (int j = 0; j < frame.Length; j++)
+					{
+					frame[j].X += this.Width / 4;
+					}
+
+				g1.FillPolygon ((i % 2 == 0) ? green : grey, frame);
+				g2.FillPolygon (back, frame);
+				}
+
+			// Объём
+			for (int i = 0; i < frameGreenGrey.Height; i++)
+				{
+				Pen p = new Pen (Color.FromArgb (200 - (int)(200.0 * LogoDrawerSupport.Sinus (180.0 * (double)i /
+					(double)frameGreenGrey.Height)), this.BackColor));
+				g1.DrawLine (p, 0, i, frameGreenGrey.Width, i);
+				p.Dispose ();
+				}
+
+			// Освобождение ресурсов
+			g1.Dispose ();
+			g2.Dispose ();
+			green.Dispose ();
+			grey.Dispose ();
+			back.Dispose ();
+
+			// Готово. Запуск
+			DrawingTimer.Interval = 1;
+			DrawingTimer.Enabled = true;
 			this.ShowDialog ();
 			}
 
@@ -94,7 +154,7 @@ namespace ESHQSetupStub
 #endif
 
 		/// <summary>
-		/// Конструктор. Выполняет указанное действие с указанными параметрами
+		/// Конструктор. Выполняет указанное действие с указанными параметрами в скрытом режиме
 		/// </summary>
 		/// <param name="HardWorkProcess">Выполняемый процесс</param>
 		/// <param name="Parameters">Передаваемые параметры выполнения</param>
@@ -122,7 +182,7 @@ namespace ESHQSetupStub
 		private void ProgressChanged (object sender, ProgressChangedEventArgs e)
 			{
 			// Обновление ProgressBar
-			if ((e.ProgressPercentage < 0) || (e.ProgressPercentage > 100))
+			/*if ((e.ProgressPercentage < 0) || (e.ProgressPercentage > 100))
 				{
 				MainProgress.Style = ProgressBarStyle.Marquee;
 				MainProgress.Value = 0;
@@ -131,7 +191,8 @@ namespace ESHQSetupStub
 				{
 				MainProgress.Style = ProgressBarStyle.Blocks;
 				MainProgress.Value = e.ProgressPercentage;
-				}
+				}*/
+			currentPercentage = e.ProgressPercentage;
 
 			StateLabel.Text = (string)e.UserState;
 			}
@@ -170,7 +231,7 @@ namespace ESHQSetupStub
 			// Собственно, выполняемый процесс
 			for (int i = 0; i < 100; i++)
 				{
-				System.Threading.Thread.Sleep (50);
+				System.Threading.Thread.Sleep (500);
 				((BackgroundWorker)sender).ReportProgress (i);	// Возврат прогресса
 
 				// Завершение работы, если получено требование от диалога
@@ -190,6 +251,36 @@ namespace ESHQSetupStub
 			{
 #if !SIMPLE_HWE
 			e.Cancel = !allowClose;
+			DrawingTimer.Enabled = false;
+
+			if (g != null)
+				g.Dispose ();
+			if (gp != null)
+				gp.Dispose ();
+			if (progress != null)
+				progress.Dispose ();
+			if (frameGreenGrey != null)
+				frameGreenGrey.Dispose ();
+			if (frameBack != null)
+				frameBack.Dispose ();
+#endif
+			}
+
+		// Отрисовка прогресс-бара
+		private void DrawingTimer_Tick (object sender, System.EventArgs e)
+			{
+#if !SIMPLE_HWE
+			// Отрисовка текущей позиции
+			gp.DrawImage (frameGreenGrey, currentOffset, 0);
+			gp.DrawImage (frameBack, -9 * this.Width / 4, 0);
+			gp.DrawImage (frameBack, (int)((double)(progress.Width - progress.Height / 2) / 100.0 *
+				(double)currentPercentage) - this.Width / 4, 0);
+
+			g.DrawImage (progress, 10, StateLabel.Top + StateLabel.Height + 10);
+
+			// Смещение
+			if (currentOffset++ >= -2 * this.Width / 4)
+				currentOffset = -4 * this.Width / 4;
 #endif
 			}
 		}
